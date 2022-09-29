55549ef459660b4cc4134ef845665744954b03d4
AkashNarale <akashnarale43@gmail.com>
2022-09-29 15:14:32 +0530

    empuc2

---
+++ empUC2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class empUC2
+    {
+        public static void UC2()
+        {
+            int IS_FULL_TIME = 1;
+            int empRatePerHr = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * empRatePerHr;
+            Console.WriteLine("Emp Wage is:" +empWage);
+            Console.ReadKey();
+        }
+    }
+}
