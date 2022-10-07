9d249d59032ed65bcb199399acb08ce514940c56
AkashNarale <akashnarale43@gmail.com>
2022-10-07 15:04:14 +0530

    empuc4

--- Program.cs
+++ Program.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
-    class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter Number\n 1. Employe Present absent\t 2. Employee daily wage\t 3. Part time wage");
+            empUC9 dMart = new empUC9("DMart", 20, 20, 100);
+            empUC9 Reliance = new empUC9("Reliance", 25, 25, 150);
+            dMart.compuateEmpWage();
+            Console.WriteLine(dMart.toString());
+            Reliance.compuateEmpWage();
+            Console.WriteLine(Reliance.toString());
+            Console.ReadKey();
+
+            /*Console.WriteLine("Enter Number\n  1.Employe Present absent\t  2.Employee daily wage\t  3.Part time wage\t  4.Switch Case\t  5.Wage & Working days\t 6.Max Hours\t 7.lass Method\t 8.multi Company");
             int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
             {
@@ -24,10 +32,27 @@ namespace HelloWorld
                 case 3:
                     empUC3.UC3();
                     break;
+                case 4:
+                    empUC4.UC4();
+                    break;
+                case 5:
+                    empUC5.UC5();
+                    break;
+                case 6:
+                    empUC6.UC6();
+                    break;
+                case 7:
+                    empUC7.UC7();
+                    break;
+                case 8:
+                    empUC8.UC8("Infy", 20, 17, 80);
+                    empUC8.UC8("Eberle", 5, 20, 100);
+                    break;
                 default:
                     Console.WriteLine("Invalide");
                     break;
-            }
+            }*/
+            Console.ReadKey();
         }
     }
 }
---
+++ empUC4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class empUC4
+    {
+        public static void UC4()
+        {
+            int empWage = 0;
+            const int fullTime = 2;
+            const int partTime = 1;
+            int empHr = 0;
+            int perHr = 20;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case partTime:
+                    empHr = 4;
+                    break;
+                case fullTime:
+                    empHr = 8;
+                    break;
+                default:
+                    empHr = 0;
+                    break;
+            }
+            empWage = empHr * perHr;
+            Console.WriteLine("Employee Wage is :" +empWage);
+        }
+    }
+}
